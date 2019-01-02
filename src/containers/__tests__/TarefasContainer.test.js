7d9c8c15e47e0f5eecf4759fc296481b7950851f
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-02 17:22:44 -0200

    initial commit

---
+++ src/containers/__tests__/TarefasContainer.test.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { render, cleanup } from 'react-testing-library'
+import TarefasContainer from '../TarefasContainer'
+
+afterEach(cleanup)
+describe('Tarefas', () => {
+	test('renderiza uma lista de tarefas a partir de um array de tarefas', () => {
+		//Arrange
+		const props = {
+			tarefas: [
+				{
+					titulo: 'Fazer o quadro Kanban',
+					conteudo: 'Fazer o quadro Kanban com react, redux etc',
+					data: '02/01/2019',
+					status: 'a fazer',
+					background: 'text-white bg-primary',
+					btnBg: 'btn-light',
+				},
+				{
+					titulo: 'Adiconar o redux',
+					conteudo: 'Adicionar o redux ao projeto',
+					data: '02/01/2019',
+					status: 'a fazer',
+					background: 'text-white bg-primary',
+					btnBg: 'btn-light',
+				},
+				{
+					titulo: 'Fazer o backend',
+					conteudo: 'fazer o servidor backend do projeto',
+					data: '02/01/2019',
+					status: 'a fazer',
+					background: 'text-white bg-primary',
+					btnBg: 'btn-light',
+				},
+			],
+		}
+		//Act
+		const { getByText } = render(<TarefasContainer />)
+
+		//Assert
+		const tituloNode = getByText(props.tarefas[1].titulo)
+		const conteudoNode = getByText(props.tarefas[1].conteudo)
+		const dataNode = getByText(props.tarefas[1].data)
+
+		expect(tituloNode).toBeDefined()
+		expect(conteudoNode).toBeDefined()
+		expect(dataNode).toBeDefined()
+	})
+})

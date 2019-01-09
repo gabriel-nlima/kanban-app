36afee996c4f49c4015430107cd0174948b6a2f5
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-09 11:22:19 -0200

    design do quadro kanban e add badges para contar as tarefas

--- src/components/AddTarefa.jsx
+++ src/components/AddTarefa.jsx
@@ -2,42 +2,44 @@ import React from 'react'
 
 const AddTarefa = ({ handleSubmit, handleChange }) => {
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className='form-row'>
-				<div className='col-3'>
-					<input
-						name='titulo'
-						type='text'
-						className='form-control mb-2 mr-sm-2'
-						onChange={handleChange}
-						placeholder='Titulo da tarefa'
-					/>
+		<div className='row'>
+			<form onSubmit={handleSubmit}>
+				<div className='form-row'>
+					<div className='col-3'>
+						<input
+							name='titulo'
+							type='text'
+							className='form-control mb-2 mr-sm-2'
+							onChange={handleChange}
+							placeholder='Titulo da tarefa'
+						/>
+					</div>
+					<div className='col-3'>
+						<input
+							name='conteudo'
+							type='text'
+							className='form-control mb-2 mr-sm-2'
+							onChange={handleChange}
+							placeholder='Conteudo'
+						/>
+					</div>
+					<div className='col-3'>
+						<input
+							name='data'
+							type='date'
+							className='form-control mb-2 mr-sm-2'
+							onChange={handleChange}
+							placeholder='Data'
+						/>
+					</div>
+					<div className='col-3'>
+						<button type='submit' className='btn btn-primary'>
+							Adicionar tarefa
+						</button>
+					</div>
 				</div>
-				<div className='col-3'>
-					<input
-						name='conteudo'
-						type='text'
-						className='form-control mb-2 mr-sm-2'
-						onChange={handleChange}
-						placeholder='Conteudo'
-					/>
-				</div>
-				<div className='col-3'>
-					<input
-						name='data'
-						type='date'
-						className='form-control mb-2 mr-sm-2'
-						onChange={handleChange}
-						placeholder='Data'
-					/>
-				</div>
-				<div className='col-3'>
-					<button type='submit' className='btn btn-primary'>
-						Adicionar tarefa
-					</button>
-				</div>
-			</div>
-		</form>
+			</form>
+		</div>
 	)
 }
 

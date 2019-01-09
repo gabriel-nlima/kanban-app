05d6035397e550a10b1dd199018ccfb486ad210d
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-09 10:13:08 -0200

    add Header, mudando containers

---
+++ packages/fastify/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className='text-left'>
+			<h3>Quadro Kanban</h3>
+			<h4>Adicione e modifique tarefas</h4>
+			<h5 className='text-muted'>AJAX?</h5>
+		</header>
+	)
+}
+
+export default Header

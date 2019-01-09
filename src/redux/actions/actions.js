2f99efdc3207cfde2d92c2e3ab4397c7b0a8e99f
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-09 09:30:55 -0200

    organizando o redux

---
+++ src/redux/actions/actions.js
@@ -0,0 +1,31 @@
+import * as actionsTypes from './actionTypes'
+import * as status from '../../containers/status'
+
+export function setTarefas() {
+	return {
+		type: actionsTypes.SET_TAREFAS,
+	}
+}
+
+export function addTarefa(tarefa) {
+	tarefa = {
+		...tarefa,
+		id: Math.floor(Math.random() * 100 + 1),
+		status: status.FAZER,
+	}
+	return {
+		type: actionsTypes.ADD_TAREFA,
+		tarefa,
+	}
+}
+
+export function trocaStatus(tarefa, novoStatus) {
+	tarefa = {
+		...tarefa,
+		status: novoStatus,
+	}
+	return {
+		type: actionsTypes.TROCA_STATUS,
+		tarefa,
+	}
+}

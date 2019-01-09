297594623e97076015b833a9d1b0f775906b2529
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-09 11:22:19 -0200

    design do quadro kanban e add badges para contar as tarefas

--- packages/kanban-app/src/components/Header.jsx
+++ packages/kanban-app/src/components/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Header = () => {
 	return (
-		<header className='text-left'>
-			<h3>Quadro Kanban</h3>
-			<h4>Adicione e modifique tarefas</h4>
-			<h5 className='text-muted'>AJAX?</h5>
-		</header>
+		<div className='row'>
+			<header className='text-left'>
+				<h3>Quadro Kanban</h3>
+				<h4>Adicione e modifique tarefas</h4>
+			</header>
+		</div>
 	)
 }
 

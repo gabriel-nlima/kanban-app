8d71e837f5d1a473796961469b8fcbe084aeb79a
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-11 15:55:27 -0200

    pagina de tarefas arquivadas e react router

--- packages/fastify/packages/kanban-app/src/index.js
+++ packages/fastify/packages/kanban-app/src/index.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import App from './App'
 import configureStore from './redux/store/configureStore'
 import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker'
+import ArquivadasContainer from './containers/ArquivadasContainer'
+import TarefasContainer from './containers/TarefasContainer'
+import Header from './components/Header'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const store = configureStore()
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<div className='container'>
+				<Header />
+				<Switch>
+					<Route path='/' exact component={App} />
+					<Route path='/tarefas' exact component={TarefasContainer} />
+					<Route
+						path='/arquivadas'
+						exact
+						component={ArquivadasContainer}
+					/>
+				</Switch>
+			</div>
+		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root')
 )

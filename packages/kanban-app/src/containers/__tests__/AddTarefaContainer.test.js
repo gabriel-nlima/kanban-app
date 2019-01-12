5142d0be99f2c9a8bb68e03a3e638ea7d8ea15dc
gabriel.lima <gabriel.lima13372@gmail.com>
2019-01-12 13:32:06 -0200

    corrigindo testes

--- packages/kanban-app/src/containers/__tests__/AddTarefaContainer.test.js
+++ packages/kanban-app/src/containers/__tests__/AddTarefaContainer.test.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { render, fireEvent, cleanup } from 'react-testing-library'
 
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 import configureMockStore from 'redux-mock-store'
-import { ADD_TAREFA } from '../../redux/actions/actionTypes'
+import { ADD_TAREFA, STARTED } from '../../redux/actions/actionTypes'
 
 import AddTarefaContainer from '../AddTarefaContainer'
 
-const mockStore = configureMockStore()
+const middlewares = [thunk] // add your middlewares like `redux-thunk`
+const mockStore = configureMockStore(middlewares)
 const store = mockStore({ tarefas: [] })
 
 function renderWithRedux(ui, { initialState, store }) {
@@ -34,9 +36,9 @@ const setup = () => {
 }
 afterEach(cleanup)
 describe('AddTarefasContainer', () => {
-	test('renderiza o formulario, insere uma tarefa e verifica se action ADD_TAREFA foi despachada', () => {
+	test('renderiza o formulario, insere uma tarefa e verifica se action STARTED foi despachada', () => {
 		//A tarefa enviada não será igual devido a data e id
-		const expectedActions = { type: ADD_TAREFA, tarefa: {} }
+		const expectedActions = { type: STARTED }
 		const { inputTitulo, inputConteudo, btnAdicionar } = setup()
 		fireEvent.change(inputTitulo, { target: { value: 'Fazer algo' } })
 		fireEvent.change(inputConteudo, {

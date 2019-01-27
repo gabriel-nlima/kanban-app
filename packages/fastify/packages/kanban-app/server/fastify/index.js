c14874eb7f364c6e22ecee44f4babc346d6d2baf
gabriel.lima <gabriel.lima13372@gmail.com>
2019-01-27 19:21:43 -0200

    add propTypes em todos os componentes

--- packages/fastify/packages/kanban-app/server/fastify/index.js
+++ packages/fastify/packages/kanban-app/server/fastify/index.js
@@ -21,7 +21,6 @@ const start = async () => {
 	try {
 		await fastify.listen(5000)
 		fastify.swagger()
-		fastify.log.info(`listening on ${fastify.server.address().port}`)
 	} catch (err) {
 		fastify.log.error(err)
 		process.exit(1)

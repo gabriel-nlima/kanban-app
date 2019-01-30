ecea68ea8e7a7381d861b95ca613b196b7d444ca
Gabriel Lima <gabriel.lima13372@gmail.com>
2019-01-30 17:28:21 -0200

    add drag and drop de tarefas

--- packages/fastify/src/components/Tarefas.jsx
+++ packages/fastify/src/components/Tarefas.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Tarefa from './Tarefa'
+import { CONCLUIDO } from '../containers/status'
 
+const onDragStart = (e, tarefa) => {
+	e.dropEffect = 'move'
+	const tarefaJson = JSON.stringify(tarefa)
+	e.dataTransfer.setData('tarefa', tarefaJson)
+}
 const Tarefas = ({ tarefas, background, acao, onClickAction }) =>
 	tarefas.map((tarefa, index) => {
 		return (
 			<div
 				key={index}
+				draggable={tarefa.status === CONCLUIDO ? false : true}
+				onDragStart={(e) => onDragStart(e, tarefa)}
 				className={'card w-100 h-auto ' + background}
 				style={{ marginBottom: 5 }}
 			>

11863c3c19e15acbbf5a5949d8b400c3ba9dff89
Gabriel Lima <gabriellima@snews.tv>
2019-02-01 18:25:02 -0200

    pequenas mudanças

--- packages/kanban-app/src/components/NoMatch.jsx
+++ packages/kanban-app/src/components/NoMatch.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom'
 const NoMatch = ({ location }) => {
 	return (
 		<div className='row'>
-			<header className='text-left'>
-				<h4 className='text-danger'>
-					URL {location.pathname} não encontrada.
-				</h4>
-				<Link to='/' className='btn btn-primary'>
-					Ir para página inicial
-				</Link>
-			</header>
+			<div className='col-12'>
+				<div className='alert alert-danger' role='alert'>
+					URL {location.pathname} não encontrada.{' '}
+					<Link className='alert-link' to='/'>
+						Ir para página inicial
+					</Link>
+				</div>
+			</div>
 		</div>
 	)
 }
